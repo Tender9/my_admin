9067a3dacd67de51a1d4bd1f8a857bc82e45948d
Tender9 <1505113506@qq.com>
2025-12-11 14:09:38 +0800

    阶段一：线上环境调试正常.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
 <template>
-   <div>当前路径：{{ $route.path }}</div>
-   <div>当前路由名称：{{ $route.name }}</div>
-   <div>当前参数：{{ $route.params }}</div>
    <a-config-provider
       :theme="{
          algorithm: theme.darkAlgorithm,
